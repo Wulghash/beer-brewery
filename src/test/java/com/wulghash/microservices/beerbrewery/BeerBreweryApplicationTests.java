dfd30c499a9f56c9411073a76e148569f2ec7301
DmytroMikhieiev <d.mikheiev@andersenlab.com>
2020-03-12 10:41:34 +0300

    Initial commit

---
+++ src/test/java/com/wulghash/microservices/beerbrewery/BeerBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wulghash.microservices.beerbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerBreweryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
